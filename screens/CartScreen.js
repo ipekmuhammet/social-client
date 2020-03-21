a67559239496e09456b305b78de4dca1e79e960d
ipekmuhammet <muhammetipek57@hotmail.com>
2020-03-21 03:12:32 +0300

    empty cart screen

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { View, FlatList, Text, StyleSheet } from 'react-native'
+import { FlatList, View, TouchableOpacity, Text, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
+import { Ionicons } from '@expo/vector-icons'
 
 import CardProduct from '../components/CardProduct'
 import CompletePayment from '../components/CompletePayment'
@@ -25,8 +26,26 @@ const CartScreen = ({ cart, refreshCard, navigation }) => {
         )
     } else {
         return (
-            <View style={styles.centeredContainer}>
-                <Text>Empty Cart!</Text>
+            <View style={styles.emptyCartContainer}>
+                <View style={styles.child} />
+                <View style={styles.child} />
+                <View style={styles.child} />
+                <View style={styles.child}>
+                    <Ionicons name={'md-basket'} size={96} color={'#BDBDBD'} />
+                </View>
+                <View style={styles.child} />
+                <View style={styles.child}>
+                    <Text style={{ fontSize: 18, textAlign: 'center' }}>Sepetinizde ürün bulunmamaktadır.</Text>
+                </View>
+                <View style={styles.child} />
+                <View style={[styles.child, { display: 'flex' }]}>
+                    <TouchableOpacity onPress={() => { navigation.navigate('products') }} style={{ backgroundColor: '#4CAB51', borderRadius: 32, alignItems: 'center', justifyContent: 'center', margin: 18, padding: 18, paddingHorizontal: 48 }}>
+                        <Text style={{ color: 'white', fontSize: 20, alignItems: 'center', justifyContent: 'center' }}>ÜRÜNLERİ LİSTELE</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.child} />
+                <View style={styles.child} />
+                <View style={styles.child} />
             </View>
         )
     }
@@ -34,7 +53,8 @@ const CartScreen = ({ cart, refreshCard, navigation }) => {
 
 const styles = StyleSheet.create({
     container: { flex: 1, backgroundColor: 'white' },
-    centeredContainer: { flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' }
+    emptyCartContainer: { flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#EDEDED' },
+    child: { flex: 1, alignItems: 'center', justifyContent: 'center' }
 })
 
 const mapStateToProps = ({
