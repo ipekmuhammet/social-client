de2b2371e799d32a6350aa583f89b21b0f9e74d0
ipekmuhammet <muhammetipek57@hotmail.com>
2020-04-03 01:27:46 +0300

    online payment screen review

--- screens/OnlinePaymentScreen.js
+++ screens/OnlinePaymentScreen.js
@@ -10,9 +10,9 @@ import { makeOrder } from '../actions/actions1'
 class OnlinePaymentScreen extends React.PureComponent {
 
     state = {
-        cardNumber: '',
-        expirationDate: '',
-        CVC2: ''
+        cardNumber: '4444 4444 4444 4444',
+        expirationDate: '09/23',
+        CVC2: '333'
     }
 
     makeOrder = () => {
@@ -27,66 +27,66 @@ class OnlinePaymentScreen extends React.PureComponent {
         return (
             <View style={styles.container}>
 
-                <View style={{
-                    flex: .16, backgroundColor: 'white', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-                    shadowColor: '#000', shadowOffset: { width: 0, height: 4, }, shadowOpacity: 0.32, shadowRadius: 6, elevation: 9
-                }}>
+                <View style={styles.paymentHeader}>
                     <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-                        <Text style={{ color: 'black', fontWeight: 'bold', fontSize: 20 }}>Ödeme Tutarı</Text>
+                        <Text style={styles.totalPriceText}>Ödeme Tutarı</Text>
                     </View>
                     <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-                        <Text style={{ color: 'black', fontWeight: 'bold', fontSize: 24 }}>{`${totalPrice} TL`}</Text>
+                        <Text style={styles.totalPrice}>{`${totalPrice} TL`}</Text>
                     </View>
                 </View>
 
 
-                <View style={{ flex: .3 }}>
-                    <View style={{ flex: 1, marginHorizontal: 12 }}>
+                <View style={styles.cardInformationContainer}>
+                    <View style={styles.inputContainer}>
                         <FloatingInput
                             labelStyle={styles.labelInput}
                             inputStyle={styles.input}
                             style={styles.formInput}
-                            value='john@email.com'>Kart Numarası *</FloatingInput>
+                            onChangeText={(cardNumber) => this.setState({ cardNumber })}
+                            value={this.state.cardNumber}>Kart Numarası *</FloatingInput>
                     </View>
 
-                    <View style={{ flex: 1, flexDirection: 'row', marginHorizontal: 12 }}>
-                        <View style={{ flex: 1 }}>
+                    <View style={styles.rowInputContainer}>
+                        <View style={styles.inputContainerChild}>
                             <FloatingInput
                                 labelStyle={styles.labelInput}
                                 inputStyle={styles.input}
                                 style={styles.formInput}
-                                value='john@email.com'>Son Kullanma Tarihi *</FloatingInput>
+                                onChangeText={(expirationDate) => this.setState({ expirationDate })}
+                                value={this.state.expirationDate}>Son Kullanma Tarihi *</FloatingInput>
                         </View>
 
-                        <View style={{ flex: 1 }}>
+                        <View style={styles.inputContainerChild}>
                             <FloatingInput
                                 labelStyle={styles.labelInput}
                                 inputStyle={styles.input}
                                 style={styles.formInput}
-                                value='john@email.com'>Güvenlik Kodu (CVC2) *</FloatingInput>
+                                onChangeText={(CVC2) => this.setState({ CVC2 })}
+                                value={this.state.CVC2}>Güvenlik Kodu (CVC2) *</FloatingInput>
                         </View>
                     </View>
                 </View>
 
-                <View style={{ flex: .1, flexDirection: 'row' }}>
-                    <View style={{ flex: 1 }} />
-                    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                        <Text style={{ fontSize: 14, color: '#A5A5A5' }}>Kartınızın arka yüzünde bulunan 3 haneli sayı</Text>
+                <View style={styles.cvcInfoContainer}>
+                    <View style={styles.child} />
+                    <View style={styles.cvcInfoTextContainer}>
+                        <Text style={styles.cvcInfoText}>Kartınızın arka yüzünde bulunan 3 haneli sayı</Text>
                     </View>
                 </View>
 
-                <View style={{ alignItems: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'row', flex: .1 }}>
-                    <View style={{ flex: 1 }} />
-                    <View style={{ flex: 7, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                        <View style={{ margin: 8 }}><Ionicons name={'md-information-circle-outline'} size={28} /></View>
+                <View style={styles.informationContainer}>
+                    <View style={styles.child} />
+                    <View style={styles.informationTextContainer}>
+                        <View style={styles.infoIconContainer}><Ionicons name={'md-information-circle-outline'} size={28} /></View>
                         <Text>Kredi kartı bilgileriniz Platform App tarafından tutulmamaktadır ödeme altyapısı Mastercard tarafından sağlanmaktadır.</Text>
                     </View>
-                    <View style={{ flex: 1 }} />
+                    <View style={styles.child} />
                 </View>
 
                 <TouchableOpacity onPress={this.makeOrder}
-                    style={{ alignItems: 'center', justifyContent: 'center', flex: .6, backgroundColor: '#D3D3D3', margin: 8, borderRadius: 36, flex: .1 }}>
-                    <Text style={{ fontSize: 22, fontWeight: 'bold', color: '#8D8D8D' }}>Siparişi Tamamla</Text>
+                    style={styles.completePaymentContainer}>
+                    <Text style={styles.completePaymentText}>Siparişi Tamamla</Text>
                 </TouchableOpacity>
             </View >
         )
@@ -95,25 +95,30 @@ class OnlinePaymentScreen extends React.PureComponent {
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'column',
-        top: 0,
-        left: 0,
-        height: Dimensions.get('window').height,
-        width: Dimensions.get('window').width,
-        backgroundColor: '#EDEDED',
-        display: 'flex',
-        flexDirection: 'column'
+        flexDirection: 'column', top: 0, left: 0, height: Dimensions.get('window').height, width: Dimensions.get('window').width,
+        backgroundColor: '#EDEDED', display: 'flex', flexDirection: 'column'
     },
-    labelInput: {
-        color: '#57A25A',
-        fontSize: 15
+    paymentHeader: {
+        flex: .16, backgroundColor: 'white', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+        shadowColor: '#000', shadowOffset: { width: 0, height: 4, }, shadowOpacity: 0.32, shadowRadius: 6, elevation: 9
     },
-    input: {
-        borderWidth: 0,
-        borderBottomWidth: 1.5,
-        borderColor: '#333',
-        color: 'black'
-    }
+    labelInput: { color: '#57A25A', fontSize: 15 },
+    inputContainerChild: { flex: 1 },
+    input: { borderWidth: 0, borderBottomWidth: 1.5, borderColor: '#333', color: 'black' },
+    totalPriceText: { color: 'black', fontWeight: 'bold', fontSize: 20 },
+    totalPrice: { color: 'black', fontWeight: 'bold', fontSize: 24 },
+    cardInformationContainer: { flex: .3 },
+    inputContainer: { flex: 1, marginHorizontal: 12 },
+    rowInputContainer: { flex: 1, flexDirection: 'row', marginHorizontal: 12 },
+    child: { flex: 1 },
+    completePaymentContainer: { alignItems: 'center', justifyContent: 'center', flex: .6, backgroundColor: '#D3D3D3', margin: 8, borderRadius: 36, flex: .1 },
+    completePaymentText: { fontSize: 22, fontWeight: 'bold', color: '#8D8D8D' },
+    cvcInfoContainer: { flex: .1, flexDirection: 'row' },
+    infoIconContainer: { margin: 8 },
+    cvcInfoTextContainer: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+    cvcInfoText: { fontSize: 14, color: '#A5A5A5' },
+    informationContainer: { alignItems: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'row', flex: .1 },
+    informationTextContainer: { flex: 7, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }
 })
 
 const mapStateToProps = ({
