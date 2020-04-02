96717bd04845f8c6ad542eaaf81bd93a76578a36
ipekmuhammet <muhammetipek57@hotmail.com>
2020-04-03 01:03:39 +0300

    review search screen

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -6,36 +6,45 @@ import HeadingDivider from '../components/HeadingDivider'
 
 const SearchScreen = () => (
     <View style={styles.container} behavior={'height'}>
-        <View style={{ flex: .8, margin: 2, flexDirection: 'row', backgroundColor: 'white' }}>
-            <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+        <View style={styles.searchHeader}>
+            <View style={styles.iconContainer}>
                 <Ionicons name={'md-search'} size={32} color={'#5D3EBD'} />
             </View>
-            <View style={{ alignItems: 'center', justifyContent: 'center', flex: 6, display: 'flex', flexDirection: 'row' }}>
-                <TextInput style={{ textAlign: 'left', flex: 1, fontSize: 21 }} placeholder={'Search Product'} />
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.searchInput} placeholder={'Search Product'} />
             </View>
-            <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+            <View style={styles.iconContainer}>
                 <Ionicons name={'md-microphone'} size={32} color={'#6D7891'} />
             </View>
         </View>
         <HeadingDivider title={'Popular Searches'} />
-        <View style={{ flex: .8, padding: 6, margin: 2, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' }}>
+        <View style={styles.mostSearchContainer}>
             <FlatList
                 data={['water', 'milk', 'bread', 'egg', 'yogurt', 'coffee']}
+                keyExtractor={(_, index) => 'most_searched' + index}
                 horizontal={true}
                 showsHorizontalScrollIndicator={false}
                 renderItem={({ item }) =>
-                    <TouchableOpacity style={{ flex: 1, paddingHorizontal: 12, margin: 4, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center', borderWidth: .4, borderColor: '#5D3EBD', borderRadius: 8 }}>
-                        <Text style={{ color: '#5D3EBD', fontSize: 14 }}>{item}</Text>
+                    <TouchableOpacity style={styles.mostSearchedItem}>
+                        <Text style={styles.itemText}>{item}</Text>
                     </TouchableOpacity>
                 }
             />
         </View>
-        <View style={{ flex: 7, margin: 2 }}></View>
+        <View style={styles.emptyFooter} />
     </View>
 )
 
 const styles = StyleSheet.create({
-    container: { top: 0, left: 0, height: Dimensions.get('window').height, width: Dimensions.get('window').width, flexDirection: 'column' }
+    container: { top: 0, left: 0, height: Dimensions.get('window').height, width: Dimensions.get('window').width, flexDirection: 'column' },
+    searchHeader: { flex: .8, margin: 2, flexDirection: 'row', backgroundColor: 'white' },
+    mostSearchContainer: { flex: .7, padding: 2, margin: 2, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' },
+    iconContainer: { alignItems: 'center', justifyContent: 'center', flex: 1 },
+    inputContainer: { alignItems: 'center', justifyContent: 'center', flex: 6, display: 'flex', flexDirection: 'row' },
+    searchInput: { textAlign: 'left', flex: 1, fontSize: 21 },
+    mostSearchedItem: { flex: 1, paddingHorizontal: 12, margin: 4, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center', borderWidth: .4, borderColor: '#5D3EBD', borderRadius: 8 },
+    itemText: { color: '#5D3EBD', fontSize: 14 },
+    emptyFooter: { flex: 7, margin: 2 }
 })
 
 export default SearchScreen
